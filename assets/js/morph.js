3c9de1c4daf56c6ce57328cd767282be24bab387
ryan-mai <ryanmai757@gmail.com>
2025-10-31 18:07:48 -0400

    Ok so i made a workingish morph image, where essentially the pixels of the image you placed are affected by some brightness which essentially makes it so you can combine the two images. i realized what i was looking for in this is much more advanced :<

--- assets/js/morph.js
+++ assets/js/morph.js
@@ -1,7 +1,7 @@
 class MorphManager {
     constructor() {
         this.canvas = document.getElementById('output');
-        this.ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext('2d', { willReadFrequently: true});
         this.upload = document.getElementById('upload');
 
         this.init();
@@ -20,7 +20,6 @@ class MorphManager {
             reader.onload = (e) => {
                 const uploadImg = new Image();
                 uploadImg.onload = () => {
-                    console.log(uploadImg);
                     this.processImg(uploadImg, this.targetImg);
                 };
                 uploadImg.src = e.target.result;
@@ -51,37 +50,49 @@ class MorphManager {
         const inputPx = inputData.data;
         const targetPx = targetData.data;
 
-        this.drawImg(inputPx, targetPx);
+        this.drawImg(inputPx, targetPx, size, 0.9267);
     }
 
-    drawImg(inputPx, targetPx) {
-        const pixels = [];
-
-        for (let i = 0; i < inputPx.length; i += 4) {
-            const avgBrightness = (inputPx[i] + inputPx[i + 1] + inputPx[i + 2]) / 3;
-            
-            pixels.push({
-                r: targetPx[i], g: targetPx[i + 1], b: targetPx[i + 2], index: i, avg: avgBrightness
-            });
-
-        }
-
-        // pixels.sort((a, b) => a.avg - b.avg);
-
+    drawImg(inputPx, targetPx, size, intensity = 0.65) {
         const output = this.ctx.createImageData(size, size);
         const outputData = output.data;
 
-        for (let q = 0; q < pixels.length; q++) {
-            const px = pixels[i];
-            const idx = px.index;
+        for (let i = 0; i < targetPx.length; i += 4) {
+            const inR = inputPx[i];            
+            const inG = inputPx[i + 1];            
+            const inB = inputPx[i + 2];            
 
-            outputData[idx] = px.r;
-            outputData[idx + 1] = px.g;
-            outputData[idx + 2] = px.b;
-            outputData[idx + 3] = 125;
+            const tR = targetPx[i];            
+            const tG = targetPx[i + 1];            
+            const tB = targetPx[i + 2];            
+
+            const [r, g, b] = this.calcBrightness(inR, inG, inB, tR, tG, tB, intensity);
+
+            outputData[i] = r;
+            outputData[i + 1] = g;
+            outputData[i + 2] = b;
+            outputData[i + 3] = 255;
         }
-
+        
         this.ctx.putImageData(output, 0, 0);
+    }
+
+    calcBrightness(inR, inG, inB, tR, tG, tB, intensity = 0.65) {
+        const luminance = (r, g, b) => (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255;
+        
+        const lum = luminance(inR, inG, inB);
+
+        const k = (lum - 0.5) * 2;
+        let factor = 1 + k * intensity;
+
+        if (factor < 1) factor = Math.pow(factor, 1.05);
+        else factor = Math.pow(factor, 0.95);
+
+        const r = Math.max(0, Math.min(255, tR * factor));
+        const g = Math.max(0, Math.min(255, tG * factor));
+        const b = Math.max(0, Math.min(255, tB * factor));
+
+        return [r, g, b];
     }
 }
 
